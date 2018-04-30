8d9addcfff445d2e43bde5fed720a3d431aa5d10
fuckomako <shnoop87@yandex.ru>
2018-04-30 22:32:40 +0300

    #26 Личный проект: перламутровые пуговицы

--- js/pin.js
+++ js/pin.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  window.renderMapPin = function (offer) {
+  window.renderMapPin = function (offer, isVisible) {
     var map = document.querySelector('.map');
     var template = document.querySelector('template').content;
     var mapPin = template.querySelector('.map__pin').cloneNode(true);
@@ -9,6 +9,7 @@
 
     mapPin.style.left = (offer.location.x - 25) + 'px';
     mapPin.style.top = (offer.location.y - 70) + 'px';
+    mapPin.style.display = isVisible ? 'block' : 'none';
     mapPinImage.src = offer.author.avatar;
     mapPinImage.alt = offer.offer.title;
     mapPin.addEventListener('click', function () {
@@ -17,7 +18,7 @@
       }
       window.card.showCard(offer);
     });
-
+    offer.mapPin = mapPin;
     return mapPin;
   };
 
